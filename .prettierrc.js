270d669d40b537185352e5b13de11d64590096aa
ldu1020 <ldu1020@gmail.com>
2024-06-12 13:47:24 +0900

    feat: 🎸setup husky

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+const base = require('@toktokhan-fe/prettier-config/base.js')
+
+/** @type {import("prettier").Config} */
+const config = {
+  ...base,
+}
+
+module.exports = config
--- src/types/module/react-query/wrap-variables.ts
+++ src/types/module/react-query/wrap-variables.ts
@@ -1,3 +1,2 @@
-export type WrapVariables<T> = undefined extends T
-  ? { variables?: T }
-  : { variables: T };
+export type WrapVariables<T> =
+  undefined extends T ? { variables?: T } : { variables: T }
